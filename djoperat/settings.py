28bf7a44ce8b65c2e1988e9cf838ca04c96ba4db
Adam <33165258+adamj2k@users.noreply.github.com>
2022-02-19 21:09:50 +0100

    Update settings.py

--- djoperat/settings.py
+++ djoperat/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #MEDIA  in temp
 MEDIA_URL = '/temp/'
 MEDIA_ROOT = 'c:/Users/Adam/djoperat/djoperat/temp/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
