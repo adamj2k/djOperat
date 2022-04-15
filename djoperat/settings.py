0b7863537b3f1f98ab9555611ddb4e9a57df5a56
Adam <33165258+adamj2k@users.noreply.github.com>
2022-04-15 20:14:38 +0200

    Update settings.py

--- djoperat/settings.py
+++ djoperat/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-75^2=1#-fb5k1a)*cn69l9_@oj@w^5)lixdgn!u1728h3nx95h'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
